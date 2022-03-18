b7e8411f089937015df1e34e6b82b63dc28edb2c
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2022-03-18 19:47:25 +0100

    first  commit

---
+++ src/main/java/io/oferto/poctrainingdockernodejsbackend/PocTrainingDockerNodejsBackendApplication.java
@@ -0,0 +1,13 @@
+package io.oferto.poctrainingdockernodejsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocTrainingDockerNodejsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocTrainingDockerNodejsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/oferto/poctrainingdockernodejsbackend/PocTrainingDockerNodejsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.oferto.poctrainingdockernodejsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocTrainingDockerNodejsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
